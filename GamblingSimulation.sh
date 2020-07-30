6b08a4f4e7530c5229c95fb9c66cf9ee434a7afb
Shreya Swaroop <shreyaswrp@gmail.com>
2020-07-30 21:04:58 +0530

    added:Gambling Simulation Program

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,23 +1,4 @@
 #! /bin/bash -x
 
 stake=100
-gameamount=0
-
-Won=0
-Lost=0
-
-while [ $gameamount -lt $stake ]
-do
-	((gameamount++))
-	result=$(($RANDOM%2))
-	if [[ $result == 0 ]]
-	then
-	((Lost++))
-	else [[ $result == 1 ]]
-	((Won++))
-	fi
-done
-
-
-echo Won $Won times
-echo Lost $Lost times
+betamount=1
