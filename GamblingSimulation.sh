e7f581c7f34ca04f3156cf4f8d1dd5ae3463fdbc
Shreya Swaroop <shreyaswrp@gmail.com>
2020-07-30 21:35:58 +0530

    updated the code to add or substract bet amount

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -2,3 +2,16 @@
 
 stake=100
 betamount=1
+
+#Calculating the result if won or lost using RANDOM function
+result=$(($RANDOM%2))
+if [[ $result == 1 ]]
+then
+	stake=$((stake+betamount))
+	echo "Won the game"
+else
+	stake=$((stake-betamount))
+	echo "Lost the game"
+fi
+
+
