c5c3f32bbb47de04c5cc460d9303f5b21cb084b0
Shreya Swaroop <shreyaswrp@gmail.com>
2020-07-31 21:06:36 +0530

    Refactor:to see days won or lost monthly

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,25 +1,16 @@
-#!/bin/bash -x
-
 stake=100
 betamount=1
 
-amountwonaday=0
-amountlostaday=0
+totalamountwon=0
+totalamountlost=0
 
 maxAmountOfDay=$(( $stake + ($stake*50)/100 | bc ));
 minAmountOfDay=$(($stake - ($stake * 50)/100 | bc ));
 
-stakeForTheDay=$stake
-
-
 function play () {
 
-
-
-        while [ $stakeForTheDay != $maxAmountOfDay ] && [ $stakeForTheDay != $minAmountOfDay ]
-        do
-        result=$(($RANDOM % 2))
-        if [[ $result == 1 ]]
+        result=$((RANDOM % 2))
+        if [ $result == 1 ]
         then
                 stakeForTheDay=$((stakeForTheDay+betamount))
                 ((amountwonaday++))
@@ -28,21 +19,29 @@ function play () {
                 ((amountlostaday++))
 
         fi
-        done
-	echo $amountwonaday
-	echo $amountlostaday
-
 }
+
 for((days=1;days<=30;days++))
 do
+	stakeForTheDay=$stake
+	amountwonaday=0
+	amountlostaday=0
+	numberOfGames=0
 
-	play
-	if [ $days -gt 20  ]
+	while [ $stakeForTheDay != $maxAmountOfDay ] && [ $stakeForTheDay != $minAmountOfDay ]
+	do
+		((numberOfGames++))
+		play
+	done
+
+	if [[ $amountwonaday -gt $amountlostaday ]]
 	then
-	echo amount won on day $days $amountwonaday
-	echo amount lost on day $days $amountlostaday
+		totalamountwon=$((totalamountwon+(amountwonaday-amountlostaday)))
+		echo amount won on day $days $totalamountwon
+	else
+		totalamountlost=$((totalamountlost+(amountlostaday-amountwonaday)))
+		echo amount lost on day $days $totalamountlost
 	fi
-
 done
 
 
