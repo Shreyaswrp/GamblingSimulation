dd93fd76e206cbb1ed4f1289139b9d73feaf1999
Shreya Swaroop <shreyaswrp@gmail.com>
2020-07-31 22:56:11 +0530

    Added:to get the luckiest and unluckiest day

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,50 +1,105 @@
-stake=100
-betamount=1
+#!/bin/bash -x
 
-totalamountwon=0
-totalamountlost=0
+#constants
+STAKE=100
+BETAMOUNT=1
 
-maxAmountOfDay=$(( $stake + ($stake*50)/100 | bc ));
-minAmountOfDay=$(($stake - ($stake * 50)/100 | bc ));
+#variables
+noOfWinDay=0
+noOfLossDay=0
+#arrays
+declare -A win
+declare -A loss
+count=1
+stakeForTheDay=$STAKE;
+percentStake=$(((STAKE*50)/100))
 
-function play () {
+function play(){
+	STAKE=100
+	noOfWins=0
+	noofLoss=0
+	stakeForTheDay=$STAKE
+	percentStake=$(((STAKE*50)/100))
+	while [ $STAKE -gt $((stakeForTheDay-percentStake)) ] && [ $STAKE -lt $((stakeForTheDay+percentStake)) ]
+	do
+		result=$((RANDOM%2))
 
-        result=$((RANDOM % 2))
-        if [ $result == 1 ]
-        then
-                stakeForTheDay=$((stakeForTheDay+betamount))
-                ((amountwonaday++))
-        else
-                stakeForTheDay=$((stakeForTheDay-betamount))
-                ((amountlostaday++))
-
-        fi
+		if [ $result -eq 1 ]
+		then
+			STAKE=$((STAKE + BETAMOUNT))
+			((noOfWins++))
+		else
+			STAKE=$((STAKE - BETAMOUNT))
+			((noOfLoss++))
+		fi
+	done
+	if [ $STAKE -gt $stakeForTheDay ]
+	then
+		win[$count]=$noOfWins
+		loss[$count]=$noOfLoss
+		((count++))
+		((noOfWinDay++))
+	else
+		win[$count]=$noOfWins
+		loss[$count]=$moOfLoss
+		((count++))
+		((noOfLossDay++))
+	fi
+	echo "you have done for the days bet"
 }
 
-for((days=1;days<=30;days++))
-do
-	stakeForTheDay=$stake
-	amountwonaday=0
-	amountlostaday=0
-	numberOfGames=0
-
-	while [ $stakeForTheDay != $maxAmountOfDay ] && [ $stakeForTheDay != $minAmountOfDay ]
-	do
-		((numberOfGames++))
-		play
-	done
-
-	if [[ $amountwonaday -gt $amountlostaday ]]
+function getAmount(){
+	if [ $noOfWinDay -gt $noOfLossDay ]
 	then
-		totalamountwon=$((totalamountwon+(amountwonaday-amountlostaday)))
-		echo amount won on day $days $totalamountwon
+		result=$((noOfWinDay*percentStake))
+		echo "the individual won $noOfWinDay days an amount of $result"
 	else
-		totalamountlost=$((totalamountlost+(amountlostaday-amountwonaday)))
-		echo amount lost on day $days $totalamountlost
+		result=$((noOfLossDay*percentStake))
+		echo "the individual lost $noOfLossDay days an amount of $result"
+	fi
+}
+
+function getLuckyDay(){
+
+lucky=${win[1]}
+for i in ${win[@]}
+do
+     if [[ $i -gt $lucky ]]
+     then
+        lucky="$i"
+     fi
+done
+for i in ${win[@]}
+do
+        ((countj++))
+        if [[ $i -eq $lucky ]]
+        then
+
+                echo "the day lucky day was $countj with win of about $lucky times"
+        fi
+done
+lucky=${loss[1]}
+for i in ${loss[@]}
+do
+     if [[ $i -gt $lucky ]]
+     then
+        lucky="$i"
+     fi
+done
+for i in ${loss[@]}
+do
+	((countk++))
+	if [[ $i -eq $lucky ]]
+	then
+
+		echo "the unlucky day was $countk with loss for about $lucky times"
 	fi
 done
-
-
-
-
+}
+for (( i=0;i<30;i++ ))
+do
+play
+done
+getAmount
+getLuckyDay
 
