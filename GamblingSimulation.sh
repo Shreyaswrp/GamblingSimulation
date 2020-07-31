859503fb25e779fa0bf504f6b80ce2799ec90568
Shreya Swaroop <shreyaswrp@gmail.com>
2020-07-31 15:27:17 +0530

    Refactor:to see amount won or lost after 20 days

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,22 +1,39 @@
-#! /bin/bash -x
+#!/bin/bash -x
 
 stake=100
 betamount=1
+amountwonaday=0
+amountlostaday=0
 
-#Calculating the result if won or lost using RANDOM function
-result=$(($RANDOM%2))
-if [[ $result == 1 ]]
-then
-	stake=$((stake+betamount))
-	echo "Won the game"
-else
-	stake=$((stake-betamount))
-	echo "Lost the game"
-fi
+maxAmountOfDay=$(( $stake + ($stake*50)/100 | bc ));
+minAmountOfDay=$(($stake - ($stake * 50)/100 | bc ));
 
-#condition on which resignation for the day will happen
-if [[ $stake == 150 || $stake == 50 ]]
-then
-	echo "Resign for the day"
-fi
+for((days=1;days<=30;days++))
+do
+	stakeForTheDay=$stake
+
+	while [[ $stakeForTheDay != $maxAmountOfDay && $stakeForTheDay != $minAmountOfDay ]]
+	do
+	result=$(($RANDOM % 2))
+	if [[ $result == 1 ]]
+	then
+		stakeForTheDay=$((stakeForTheDay+betamount))
+		((amountwonaday++))
+	else
+		stakeForTheDay=$((stakeForTheDay-betamount))
+		((amountlostaday++))
+
+	fi
+	done
+
+	if [[ $days -gt 20 && $days -le 30 ]]
+	then
+	echo amount won on day $days $amountwonaday
+	echo amount lost on day $days $amountlostaday
+	fi
+
+done
+
+
+echo $days
 
